1b7a4b19f3115f74fc7107662081f91336323a16
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-10 16:11:27 +0400

    Add Tite

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Header from "./Components/Header/Header"
 
 function App() {
 
   return (
-    <div className="flex flex-col justify-evenly items-center">
-      
+    <div className="flex flex-col justify-evenly items-center w-[750px] h-full mt-[60px]">
+        <Header />
 
     </div>
   )
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <div className="flex flex-col justify-evenly items-center ">
+        <h1 className="font-medium text-[26px] ">TODO LIST</h1>
+    </div>
+  )
+}
+
+export default Header
