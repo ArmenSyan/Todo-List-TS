c372a527f67ef833f7ac143ce692c15968b9713e
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-10 17:09:33 +0400

    Create Dropdown

---
+++ src/Components/Header/DropDown/Dropdown.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+function Dropdown() {
+    const [open, setOpen] = useState(false)
+
+    function button() {
+        if (open) setOpen(false)
+        else setOpen(true)
+    }
+    return (
+        <div className="relative w-[100px] ">
+            <button onClick={button}
+                className="w-full h-[38px] rounded-[5px] flex justify-between items-center cursor-pointer text-[18px] px-[10px] text-white bg-purple font-medium duration-300 transition-all">ALL  {!open ? <IoIosArrowDown /> : <IoIosArrowUp />}</button>
+
+            {open && (
+                <div className="absolute  mt-2 w-full bg-[white] text-purple border rounded-[5px] py-[5px] duration-300">
+                    <ul className="flex flex-col text-center font-medium">
+                        <li className="w-[full] cursor-pointer h-[38px] px-[10px]  flex justify-between items-center hover:bg-hover duration-300 transition-all">All</li>
+                        <li className="w-[full] cursor-pointer h-[38px] px-[10px]  flex justify-between items-center hover:bg-hover duration-300 transition-all">Complate</li>
+                        <li className="w-[full] cursor-pointer h-[38px] px-[10px]  flex justify-between items-center hover:bg-hover duration-300 transition-all">Incomplate</li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Dropdown
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,13 @@
+import Dropdown from "./DropDown/Dropdown"
 import Search from "./Search/Search"
 
 function Header() {
   return (
-    <div className="flex flex-col justify-evenly items-center ">
+    <div className="flex flex-col justify-center items-center ">
         <h1 className="font-medium text-[26px] ">TODO LIST</h1>
         <div className="flex justify-evenly items-center gap-x-2 mt-[16px]">
             <Search />
+            <Dropdown />
         </div>
     </div>
   )
