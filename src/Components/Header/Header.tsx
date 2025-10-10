40c0848bacadfd0226d6d632d65bc71f9675561c
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-10 16:33:37 +0400

    Create Search Input

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,12 @@
+import Search from "./Search/Search"
 
 function Header() {
   return (
     <div className="flex flex-col justify-evenly items-center ">
         <h1 className="font-medium text-[26px] ">TODO LIST</h1>
+        <div className="flex justify-evenly items-center gap-x-2 mt-[16px]">
+            <Search />
+        </div>
     </div>
   )
 }
