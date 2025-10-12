95fc335f82112a6ed43a32c18ae0108c0a2480a2
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-13 01:01:23 +0400

    Added shift for notes addinng modal

--- src/Components/Modals/NoteModal.tsx
+++ src/Components/Modals/NoteModal.tsx
@@ -1,7 +1,7 @@
 import { useFilter } from "../../CustomHook"
 
 function NoteModal() {
-    const { data, setData, addInputValue, setActiveModal, setAddInputValue } = useFilter()
+    const { data, setData, addInputValue, setActiveModal, setAddInputValue, darkMode } = useFilter()
 
     function setActive(): void {
         setActiveModal(false)
@@ -24,7 +24,7 @@ function NoteModal() {
     }
     return (
         <div className="fixed w-[100vw] h-[100vh] pt-[124px] flex justify-evenly  bg-black/60 -z-50-">
-            <div className=" w-[500px] h-[289px] bg-white rounded-[15px] flex flex-col justify-between items-center z-50">
+            <div className={`w-[500px] h-[289px] ${darkMode ? 'bg-black border-[1px] border-white' : 'bg-white '} rounded-[15px] flex flex-col justify-between items-center z-50`}>
                 <div className="flex flex-col justify-evenly items-center mt-[18px] gap-y-6">
                     <h1 className="text-[24px] font-medium">
                         NEW NOTE
@@ -33,10 +33,10 @@ function NoteModal() {
                         type="text"
                         onChange={(e) => inputValue(e)}
                         placeholder="Input your note..."
-                        className="w-[440px] h-[38px] border-[2px] px-[16px] rounded-[5px]  border-purple  py-[4px] text-[16px]  font-medium focus:outline-0" />
+                        className={`w-[440px] h-[38px] border-[1px] px-[16px] rounded-[5px] ${darkMode ? 'border-white' : 'bg-white '}   py-[4px] text-[16px]  font-medium focus:outline-0`}/>
                 </div>
                 <div className="flex justify-between items-center w-full px-[30px] mb-[18px]">
-                    <button onClick={setActive} className="w-[110px] h-[38px] border-[3px] border-purple rounded-[5px] text-purple font-medium text-[18px] hover:cursor-pointer hover:shadow-lg hover:shadow-purple/40 duration-200">CANCEL</button>
+                    <button onClick={setActive} className="w-[110px] h-[38px] border-[2px] border-purple rounded-[5px] text-purple font-medium text-[18px] hover:cursor-pointer hover:shadow-lg hover:shadow-purple/40 duration-200">CANCEL</button>
                     <button onClick={addNote} className="w-[110px] h-[38px] border-[3px] border-purple rounded-[5px] text-white bg-purple font-medium text-[18px] hover:cursor-pointer hover:shadow-lg hover:shadow-purple/40 duration-300">APPLY</button>
                 </div>
             </div>
